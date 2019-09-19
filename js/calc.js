e3c346ec04c414941d834bc39f16fcdb5791f979
Hunter Carey <huntercarey902@gmail.com>
2019-09-19 14:52:24 -0500

    Implemented challenges to tip calculator. Added the mealPerPerson and TipPerPerson

--- js/calc.js
+++ js/calc.js
@@ -5,7 +5,7 @@ function calculateTip() {
     var billAmount = document.getElementById("billAmount").value;
     console.log(billAmount);
 
-    var serviceQuality = document.getElementById("dropdown").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
     console.log(serviceQuality);
 
     var numberOfPeople = document.getElementById("numOfPeople").value;
@@ -43,14 +43,19 @@ function calculateTip() {
 
     //Calculate the amount of tip each person needs to pay
 
-    var tipPerPerson = (amount * serviceQuality) / people;
+    var tipPerPerson = (amount * service) / people;
     console.log(tipPerPerson);
 
     tipPerPerson = tipPerPerson.toFixed(2); //round the tip per person to 2 decimal points
     console.log(tipPerPerson);
 
     //Display that information to the user
-    var feedBackMessage = `$${tipPerPerson} each`
+    var mealPricePerPerson = amount / people;
+    mealPricePerPerson = mealPricePerPerson.toFixed(2);
+    var totalPerPerson = Number(mealPricePerPerson) + Number(tipPerPerson);
+    totalPricePerPerson = Number(totalPerPerson);
+    totalPerPerson = totalPerPerson.toFixed(2);
+    var feedBackMessage = `Tip: $${tipPerPerson} each\nMeal: $${mealPricePerPerson} each\nTotal: $${totalPerPerson} each`;
 
     document.getElementById("feedBack").innerText = feedBackMessage;
 }
@@ -61,4 +66,15 @@ function calculateTip() {
 
 document.getElementById("submit").onclick = function() {
     calculateTip();
+}
+
+function reset() {
+    document.getElementById("billAmount").value = '';
+    document.getElementById("serviceQuality").value = 0;
+    document.getElementById("numOfPeople").value = 0;
+    document.getElementById("feedBack").innerText = '';
+}
+
+document.getElementById("reset").onclick = function() {
+    reset();
 }
